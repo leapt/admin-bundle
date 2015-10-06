512032d22e4cd94722b4bdb50f201dc2e4bd65f1
jmsche <contact@jmsche.fr>
2015-10-06 15:43:05 +0200

    Fixed Twig AdminLoader

--- Twig/Loader/AdminLoader.php
+++ Twig/Loader/AdminLoader.php
@@ -4,17 +4,23 @@ namespace Leapt\AdminBundle\Twig\Loader;
 
 use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 
-class AdminLoader extends FilesystemLoader {
+/**
+ * Class AdminLoader
+ * @package Leapt\AdminBundle\Twig\Loader
+ */
+class AdminLoader extends FilesystemLoader
+{
     /**
      * @param string $template
+     * @param bool $throw
      * @return string
      */
-    protected function findTemplate($template)
+    protected function findTemplate($template, $throw = true)
     {
         $parts = explode(':', $template);
         $parts[1] = 'Content';
         $defaultTemplate = implode(':', $parts);
 
-        return parent::findTemplate($defaultTemplate);
+        return parent::findTemplate($defaultTemplate, $throw);
     }
 }
