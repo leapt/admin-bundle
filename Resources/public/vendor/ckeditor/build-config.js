e7cf75f54b56977658106455af4225ee776a5e50
jmsche <contact@jmsche.fr>
2016-03-17 16:09:19 +0100

    Updated CKEditor to 4.5.7 & added VideoDetector plugin

--- Resources/public/vendor/ckeditor/build-config.js
+++ Resources/public/vendor/ckeditor/build-config.js
@@ -1,14 +1,25 @@
-﻿
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+﻿/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
  * This file was added automatically by CKEditor builder.
- * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
- * 
- * NOTE: 
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/195c1866d6de5fbcf96424543dda6d79
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/195c1866d6de5fbcf96424543dda6d79
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
  *    This file is not used by CKEditor, you may remove it.
  *    Changing this file will not change your CKEditor configuration.
  */
@@ -17,70 +28,86 @@ var CKBUILDER_CONFIG = {
 	skin: 'moono',
 	preset: 'standard',
 	ignore: [
+		'.bender',
+		'bender.js',
+		'bender-err.log',
+		'bender-out.log',
 		'dev',
-		'.gitignore',
+		'.DS_Store',
+		'.editorconfig',
 		'.gitattributes',
+		'.gitignore',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'less',
+		'.mailmap',
+		'node_modules',
+		'package.json',
 		'README.md',
-		'.mailmap'
+		'tests'
 	],
 	plugins : {
-		'about' : 1,
 		'a11yhelp' : 1,
+		'about' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
+		'button' : 1,
 		'clipboard' : 1,
+		'codemirror' : 1,
 		'contextmenu' : 1,
-		'resize' : 1,
-		'toolbar' : 1,
+		'dialog' : 1,
+		'dialogui' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'fakeobjects' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
+		'floatpanel' : 1,
 		'format' : 1,
-		'htmlwriter' : 1,
 		'horizontalrule' : 1,
-		'wysiwygarea' : 1,
+		'htmlwriter' : 1,
+		'image2' : 1,
 		'indent' : 1,
+		'indentlist' : 1,
+		'justify' : 1,
+		'lineutils' : 1,
 		'link' : 1,
 		'list' : 1,
+		'listblock' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
-		'pastetext' : 1,
+		'menu' : 1,
+		'menubutton' : 1,
+		'panel' : 1,
 		'pastefromword' : 1,
+		'pastetext' : 1,
+		'popup' : 1,
 		'removeformat' : 1,
+		'resize' : 1,
+		'richcombo' : 1,
+		'scayt' : 1,
+		'showblocks' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
-		'scayt' : 1,
 		'stylescombo' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
-		'undo' : 1,
-		'wsc' : 1,
-		'dialog' : 1,
-		'dialogui' : 1,
-		'menu' : 1,
-		'floatpanel' : 1,
-		'panel' : 1,
-		'button' : 1,
-		'popup' : 1,
-		'richcombo' : 1,
-		'listblock' : 1,
-		'fakeobjects' : 1,
-		'menubutton' : 1,
 		'templates' : 1,
-		'codemirror' : 1,
-		'showblocks' : 1,
-		'justify' : 1,
-		'indentlist' : 1,
-		'lineutils' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'videodetector' : 1,
 		'widget' : 1,
-		'image2' : 1
+		'wsc' : 1,
+		'wysiwygarea' : 1
 	},
 	languages : {
-		'nl' : 1,
 		'en' : 1,
-		'fr' : 1
+		'fr' : 1,
+		'nl' : 1
 	}
 };
